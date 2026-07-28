76cd176f56566349b54744859fc3a660edf0bb36
Sparking+ <63219026+XuanlZhu@users.noreply.github.com>
2026-07-28 14:56:08 +0800

    1

--- easyX/ResourceManager/CAnimaManager.cpp
+++ easyX/ResourceManager/CAnimaManager.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "CAnimaManager.h"
+
+CAnimaManager::CAnimaManager() {
+
+}
+void CAnimaManager::Load(std::string path,int frameWidth,int frameHeight)
+{
+    IMAGE sheet;
+    loadimage(&sheet,path.c_str());
+
+    int width = sheet.getwidth();
+    int height = sheet.getheight();
+    int cols = width / frameWidth;
+    int rows = height / frameHeight;
+
+    for(int y = 0; y < rows; y++)
+    {
+        std::vector<IMAGE> animation;
+        for(int x = 0; x < cols; x++)
+        {
+            IMAGE frame;
+            // 创建单帧图片
+            SetWorkingImage(&sheet);
+            getimage(&frame,x * frameWidth,y * frameHeight,frameWidth,frameHeight);
+            animation.push_back(frame);
+        }
+
+        mAnimations.push_back(animation);
+    }
+    SetWorkingImage(nullptr);
+}
--- easyX/ResourceManager/CAnimaManager.h
+++ easyX/ResourceManager/CAnimaManager.h
@@ -3,10 +3,20 @@
 //
 
 #pragma once
+#include <vector>
+#include <string>
+#include <graphics.h>
 
-
-class CAnimaManager {
+class CAnimaManager
+{
 public:
-    void Load();
-
+    CAnimaManager();
+    // 加载sprite sheet
+    void Load(std::string path,int frameWidth,int frameHeight);
+    // 获取某行动画
+    std::vector<IMAGE>& GetAnimation(int row);
+    // 获取某帧
+    IMAGE& GetFrame(int row,int frame);
+private:
+    std::vector<std::vector<IMAGE>> mAnimations;
 };
